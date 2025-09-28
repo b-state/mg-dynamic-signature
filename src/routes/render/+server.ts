26cb38a343a433758ea73d6840a63e816df2e842
Philipp Benner <ppbenner@outlook.de>
2025-09-28 17:01:12 +0200

    Add server-side screenshot functionality with Puppeteer and improve SalePropertyItem component styling

---
+++ src/routes/render/+server.ts
@@ -0,0 +1,21 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+import puppeteer from 'puppeteer';
+
+export const GET: RequestHandler = async ({ url }) => {
+	const browser = await puppeteer.launch({});
+	const page = await browser.newPage();
+
+	// Load the page
+	await page.goto('http://localhost:5173', { waitUntil: 'networkidle0' });
+
+	// Take screenshot
+	await page.screenshot({
+		path: './static/image/aktuelles-angebot.jpeg',
+		type: 'jpeg',
+		quality: 80, // 0–100
+		fullPage: true
+	});
+
+	await browser.close();
+	return json({"message":"ok"});
+};

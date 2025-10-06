2d07197908a33690a399c345deb4ad25e77e7f28
Philipp Benner <ppbenner@outlook.de>
2025-10-06 21:27:34 +0200

    add port

--- src/routes/render/+server.ts
+++ src/routes/render/+server.ts
@@ -1,12 +1,13 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import puppeteer from 'puppeteer';
+import { PORT } from '$env/static/private';
 
 export const GET: RequestHandler = async () => {
 	const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
 	const page = await browser.newPage();
 
 	// Load the page
-	await page.goto('http://localhost:5173', { waitUntil: 'networkidle0' });
+	await page.goto(`http://localhost:${PORT}`, { waitUntil: 'networkidle0' });
 
 	// Take screenshot
 	await page.screenshot({
